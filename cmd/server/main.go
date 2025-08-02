cdeed961f5c9df9eff39e70f99b7c6e1b44af0e2
lealre <renaanvp@gmail.com>
2025-08-02 18:20:36 +0100

    Add REPL options in server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -23,20 +22,51 @@ func main() {
 	defer conn.Close()
 
 	fmt.Println("Connected to RabbitMQ!")
-
 	channel, err := conn.Channel()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer channel.Close()
 
-	pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
-		IsPaused: true,
-	})
+	gamelogic.PrintServerHelp()
+
+	for {
+		wordSlice := gamelogic.GetInput()
+		if len(wordSlice) == 0 {
+			continue
+		}
+
+		if wordSlice[0] == "pause" {
+			fmt.Printf("Sending pause message.\n")
+			pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: true,
+			})
+			continue
+		}
+
+		if wordSlice[0] == "resume" {
+			fmt.Printf("Sensing resume message.\n")
+			pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: false,
+			})
+			continue
+		}
+
+		if wordSlice[0] == "quit" {
+			fmt.Printf("Quitting...\n")
+			break
+		}
+
+		fmt.Println("Invalid command.")
+	}
+
+	// pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+	// 	IsPaused: true,
+	// })
 
 	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("Shutting down gracefully...")
+	// signalChan := make(chan os.Signal, 1)
+	// signal.Notify(signalChan, os.Interrupt)
+	// <-signalChan
+	// fmt.Println("Shutting down gracefully...")
 }
